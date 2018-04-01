98cc34ec06c9be22127679e3937ed490954d7487
Shruti Singh <M1039304@mindtree.com>
2018-04-01 10:54:53 +0530

    ninth commit

--- write_codedeploy_config.sh
+++ write_codedeploy_config.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p /var/codedeploy/tomcat-sample
 
 cat <<EOF >/var/codedeploy/tomcat-sample/env.properties
-APPLICATION_NAME=$APPLICATION_NAME
-DEPLOYMENT_GROUP_NAME=$DEPLOYMENT_GROUP_NAME
+APPLICATION_NAME=SampleDemoTest
+DEPLOYMENT_GROUP_NAME=CICDdemo
 DEPLOYMENT_ID=$DEPLOYMENT_ID
 EOF
